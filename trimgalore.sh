201e0753949c5ab5f6bc7951031b0b2a005a33c8
rbhawes <116133870+rbhawes@users.noreply.github.com>
2022-10-19 23:14:55 -0400

    Create trimgalore.sh

---
+++ trimgalore.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#SBATCH -A bme4350
+#SBATCH -p standard
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH -c 16
+#SBATCH -t 8:00:00
+#SBATCH --mail-type=all
+#SBATCH --mail-user=rbh2equ@virginia.edu
+
+R1_FILES=($(ls -1 ../raw/*1.fastq))
+R2_FILES=($(ls -1 ../raw/*2.fastq))
+
+R1_FILE=${R1_FILES[$SLURM_ARRAY_TASK_ID]}
+R2_FILE=${R2_FILES[$SLURM_ARRAY_TASK_ID]}
+
+echo $R1_FILE
+echo $R2_FILE
+
+module purge
+module load trimgalore
+
+trim_galore --paired --cores 4 --quality 20 --illumina --length 20 --fastqc_args "--outdir ./trimmed_fastqc" $R1_FILE $R2_FILE
